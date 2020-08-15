44b5924740940b1c6e7d0252214b90c66079030d
Fabian Isaksson <fabi.isak@gmail.com>
2020-08-15 23:22:47 +0200

    reset text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          <code>It works!</code>
+          <code>Work in progress</code>
         </p>
       </header>
     </div>
